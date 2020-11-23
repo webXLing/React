94fb54c25a7a14ba23b0a771bb133dcf574ea356
web_XL <597146220@qq.com>
2020-11-23 09:19:41 +0800

    react

--- jianshu/src/common/header/store/constants.js
+++
@@ -1,9 +0,0 @@
-export const SEARCH_BLUR = 'header/searchBlur'
-export const SEARCH_FOCUS = 'header/searchFocus'
-export const CHANGE_LIST = 'header/changeList'
-export const MOUSECHANGGE = 'header/MOUSECHANGGE'
-export const CHANGREPAGE = 'header/CHANGREPAGE'
-
-
-
-
